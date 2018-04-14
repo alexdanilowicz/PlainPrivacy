63b113e0261bbb7f551f5275c4d6063445327c28
Teddarific <teddy.ni.19@dartmouth.edu>
2018-04-14 15:09:14 -0400

    functional

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,10 @@
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete' && tab.active) {
 
-    chrome.tabs.sendMessage(tabId, {}, function(response) {
+    chrome.tabs.sendMessage(tabId, {args: {"tabId": tabId }}, function(response) {
+      if ( response ){
+        chrome.pageAction.show(tabId);
+      }
       console.log("finished running");
     });
   }
--- scripts/checkForPP.js
+++ scripts/checkForPP.js
@@ -1,29 +1,42 @@
-function checkForValidUrl(tabId, changeInfo, tab)
-{
-  console.log(document.querySelector('body'));
-    if (typeof tab != "undefined" && typeof tab != "null" ){
-            console.log(document.querySelector('body'));
-            // ... show the page action.
-            chrome.pageAction.show(tabId);
-    }
-};
+// function checkForValidUrl(tabId, changeInfo, tab)
+// {
+//   console.log(document.querySelector('body'));
+//     if (typeof tab != "undefined" && typeof tab != "null" ){
+//             console.log(document.querySelector('body'));
+//             // ... show the page action.
+//             chrome.pageAction.show(tabId);
+//     }
+// };
+//
+// function checkForPrivacyPolicy(tabId, changeInfo, tab){
+//   console.log(document.querySelector('body'));
+//   // chrome.tabs.executeScript(tabId, {file:"scripts/checkForPP.js"}, function(){
+//   //   console.log("called");
+//   // });
+// }
 
-function checkForPrivacyPolicy(tabId, changeInfo, tab){
-  console.log(document.querySelector('body'));
-  // chrome.tabs.executeScript(tabId, {file:"scripts/checkForPP.js"}, function(){
-  //   console.log("called");
-  // });
-}
+function checkForPrivacyPolicy(){
+  var anchors = document.getElementsByTagName('a');
 
-function testFunc(){
-  console.log('sup');
-  console.log(document.querySelector('body'));
+  for (var i = 0; i < anchors.length; i++) {
+      if (anchors[i].href.includes("privacy")) {
+          // console.log(anchors[i].href);
+          // console.log("True");
+          // policy url
+
+          // Code to call server will be here
+          console.log(anchors[i].href)
+          return true;
+      }
+  }
+  return false;
 }
 
 
 console.log("this script was loaded");
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    testFunc();
-    sendResponse(1);
+    let tabId = request.args.tabId;
+    let pp = checkForPrivacyPolicy();
+    sendResponse(pp);
 });
