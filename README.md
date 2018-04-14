3ed00dd6ac7811824a0a3e52284c68a572a113d3
Alexander Danilowicz <alexander.j.danilowicz.19@dartmouth.edu>
2018-04-14 14:22:51 -0400

    crawler

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# PlainPrivacy
-Plain Privacy - Chrome Extension
+# Plain Privacy
+
+![Logo](img/logo.jpg)
+
+Plain Privacy is a Google Chrome Extension built for Hack Datmouth 2018.
+
+Group: Chris Bertasi, Alexander Danilowicz, Dylan Hong, and Teddy Ni.
---
+++ crawler.js
@@ -0,0 +1,26 @@
+/* Plain Privacy  - crawler
+ * reads page,
+ * loops over all anchor tags, looking for "privacy" string
+ * returns privacy policy url is policy on page.
+ * returns False otherwise, nothing happens
+ */
+
+var grabAnchors = function () {
+      var anchors = document.getElementsByTagName('a');
+
+      for (var i = 0; i < anchors.length; i++) {
+          if (anchors[i].href.includes("privacy")) {
+              console.log(anchors[i].href);
+              console.log("True");
+              // policy url
+              return (anchors[i].href);
+          }
+          if (anchors[i].href.includes("about")) {
+            console.log(anchors[i].href);
+          }
+      }
+      console.log("Privacy policy not found.");
+      return false;
+}
+
+grabAnchors();
